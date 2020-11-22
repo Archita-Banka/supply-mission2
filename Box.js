a8e10f3e4ea9e1531cda88cc8364342e853c6ce7
Archita-Banka <73935422+Archita-Banka@users.noreply.github.com>
2020-11-22 12:04:01 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,21 @@
+class Box{
+    constructor(x,y,width,height){
+        var options = {
+            'restitution': 0.8,
+            'friction': 1.0,
+        }
+            this.body = Bodies.rectangle(x,y,width,height, options);
+            this.width = width;
+            this.height = height;
+            World.add(world,this.body);
+        }
+
+        display(){
+        
+            rectMode(CENTER);
+            strokeWeight(2);
+            stroke("white");
+            fill("red");
+            rect(this.body.position.x,this.body.position.y, this.width, this.height);
+        }
+}
